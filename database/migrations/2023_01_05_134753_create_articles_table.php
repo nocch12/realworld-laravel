6018048c3fc37b3fe7da6756cfbb5512cda27615
Kohki Ohhno <k.ohno12.gt@gmail.com>
2023-01-05 14:05:29 +0000

    migrations

---
+++ database/migrations/2023_01_05_134753_create_articles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('author_id')->constrained('users')->cascadeOnDelete();
+            $table->string('slug')->unique();
+            $table->string('title');
+            $table->text('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+};
