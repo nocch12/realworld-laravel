8a469e669eebdaacee4e8e534b3a3b03f0ee86a4
Kohki Ohhno <k.ohno12.gt@gmail.com>
2023-01-06 14:15:05 +0000

    make factory

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
+ */
+class ArticleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title'       => $this->faker->sentence(3),
+            'description' => $this->faker->sentence(10),
+            'body'        => $this->faker->text(),
+        ];
+    }
+}
